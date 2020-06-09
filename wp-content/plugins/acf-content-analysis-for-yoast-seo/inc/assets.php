dbbece82ee334f44395133ce78b2f6c026b7c225
Nikita Lobach <nikita.lobach@beetroot.se>
2020-06-09 15:05:38 +0300

    Update: ACF Content Analysis for Yoast v2.3.0 -> v2.4.0

--- wp-content/plugins/acf-content-analysis-for-yoast-seo/inc/assets.php
+++ wp-content/plugins/acf-content-analysis-for-yoast-seo/inc/assets.php
@@ -23,19 +23,17 @@ class Yoast_ACF_Analysis_Assets {
 	public function init() {
 		$this->plugin_data = get_plugin_data( AC_SEO_ACF_ANALYSIS_PLUGIN_FILE );
 
-		add_filter( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ), 11 );
+		add_filter( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts' ], 11 );
 	}
 
 	/**
 	 * Enqueue JavaScript file to feed data to Yoast Content Analyses.
 	 */
 	public function enqueue_scripts() {
-		global $pagenow;
-
 		/**
 		 * Yoast ACF plugin configuration.
 		 *
-		 * @var \Yoast_ACF_Analysis_Configuration
+		 * @var Yoast_ACF_Analysis_Configuration
 		 */
 		$config = Yoast_ACF_Analysis_Facade::get_registry()->get( 'config' );
 
@@ -44,7 +42,7 @@ class Yoast_ACF_Analysis_Assets {
 			wp_enqueue_script(
 				'yoast-acf-analysis-post',
 				plugins_url( '/js/yoast-acf-analysis.js', AC_SEO_ACF_ANALYSIS_PLUGIN_FILE ),
-				array( 'jquery', WPSEO_Admin_Asset_Manager::PREFIX . 'post-scraper', 'underscore' ),
+				[ 'jquery', WPSEO_Admin_Asset_Manager::PREFIX . 'post-scraper', 'underscore' ],
 				$this->plugin_data['Version'],
 				true
 			);
@@ -57,7 +55,7 @@ class Yoast_ACF_Analysis_Assets {
 			wp_enqueue_script(
 				'yoast-acf-analysis-term',
 				plugins_url( '/js/yoast-acf-analysis.js', AC_SEO_ACF_ANALYSIS_PLUGIN_FILE ),
-				array( 'jquery', WPSEO_Admin_Asset_Manager::PREFIX . 'term-scraper' ),
+				[ 'jquery', WPSEO_Admin_Asset_Manager::PREFIX . 'term-scraper' ],
 				$this->plugin_data['Version'],
 				true
 			);
