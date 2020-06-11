fee52a99768a624e0634b723a6762d910d256048
Nikita Lobach <nikita.lobach@beetroot.se>
2020-06-11 18:38:37 +0300

    Adjusted theme js formatting in files

--- wp-content/themes/optovicom/assets/scripts/golden-footer.js
+++ wp-content/themes/optovicom/assets/scripts/golden-footer.js
@@ -1,73 +1,82 @@
-console.log('we here');
-
 moveFilterToRight();
 moveFilterButtonDown();
 removeFilterIfProductsNoexist();
-moveitem();
+moveItem();
 addFilterShowButton();
+
 function moveFilterToRight() {
-    var products = document.querySelector('.products');
-    if (!products) return false;
-    var filter = document.querySelector('.golden-filters');
-    if (!filter) return false;
-    var main = document.querySelector('#main');
-    if (!main) return false;
-    var wrapForProducts = document.createElement('div');
-    wrapForProducts.classList.add('golden-wrap-products');
-    main.insertBefore(wrapForProducts,products);
-    wrapForProducts.appendChild(filter);
-    wrapForProducts.appendChild(products);
+  var products = document.querySelector('.products');
+  if (!products) return false;
+
+  var filter = document.querySelector('.golden-filters');
+  if (!filter) return false;
+
+  var main = document.querySelector('#main');
+  if (!main) return false;
+
+  var wrapForProducts = document.createElement('div');
+  wrapForProducts.classList.add('golden-wrap-products');
+
+  main.insertBefore(wrapForProducts, products);
+  wrapForProducts.appendChild(filter);
+  wrapForProducts.appendChild(products);
 }
+
 function moveFilterButtonDown() {
-    var priceFilter = document.querySelector('.price_slider_amount');
-    if (!priceFilter) return false;
-    var buttonPriceFilter = priceFilter.querySelector('.button');
-    if (!buttonPriceFilter) return false;
-    priceFilter.appendChild(buttonPriceFilter);
+  var priceFilter = document.querySelector('.price_slider_amount');
+  if (!priceFilter) return false;
+
+  var buttonPriceFilter = priceFilter.querySelector('.button');
+  if (!buttonPriceFilter) return false;
+
+  priceFilter.appendChild(buttonPriceFilter);
 }
+
 function removeFilterIfProductsNoexist() {
-    var noExist = document.querySelector('.woocommerce-no-results');
-    var filter = document.querySelector('.golden-filters');
-    if (!noExist || !filter) return false;
-    filter.remove();
+  var noExist = document.querySelector('.woocommerce-no-results'),
+    filter = document.querySelector('.golden-filters');
+
+  if (!noExist || !filter) return false;
+  filter.remove();
 }
-function moveitem() {
-    var wrap = document.querySelector('.golden-wrap-products');
-    if (!wrap) return false;
-    $('.golden-wrap-products').bind("DOMSubtreeModified",function(){
-        var ulForProduct = wrap.querySelector('.products');
-        var allLi = document.querySelectorAll('.golden-wrap-products > .type-product');
-        allLi.forEach(function (element) {
-            ulForProduct.appendChild(element);
-        });
+
+function moveItem() {
+  var wrap = document.querySelector('.golden-wrap-products');
+  if (!wrap) return false;
+
+  $('.golden-wrap-products').bind("DOMSubtreeModified", function () {
+    var ulForProduct = wrap.querySelector('.products'),
+      allLi = document.querySelectorAll('.golden-wrap-products > .type-product');
+
+    allLi.forEach(function (element) {
+      ulForProduct.appendChild(element);
     });
+  });
 }
+
 function addFilterShowButton() {
-    var header = document.querySelector('.golden-wrap-products');
-    var mainBlock = document.querySelector('#main');
-    if (!mainBlock) return false;
-    if (!header) return false;
-    var filterBlock = document.querySelector('.golden-filters');
-    var button = document.createElement('button');
-    button.classList.add('golden-filter-button');
-    // filterBlock.style.display = 'none';
-    // button.classList.add('golden-filter-button__active');
-    button.textContent = 'Фильтры';
-    button.addEventListener('click', function (evt) {
-        if (button.classList.contains('golden-filter-button__active')){
-            // filterBlock.style.display = 'none';
-            button.classList.remove('golden-filter-button__active');
-            filterBlock.classList.remove('golden-filters__active');
-        }
-        else {
-            filterBlock.classList.add('golden-filters__active');
-            button.classList.add('golden-filter-button__active');
-        }
-    })
+  var header = document.querySelector('.golden-wrap-products'),
+    mainBlock = document.querySelector('#main');
 
-    mainBlock.insertBefore(button, header);
-    console.log('fdsfsdfsd');
+  if (!mainBlock) return false;
+  if (!header) return false;
 
+  var filterBlock = document.querySelector('.golden-filters'),
+    button = document.createElement('button');
 
+  button.classList.add('golden-filter-button');
+  button.textContent = 'Фильтры';
 
+  button.addEventListener('click', function (evt) {
+    if (button.classList.contains('golden-filter-button__active')) {
+      button.classList.remove('golden-filter-button__active');
+      filterBlock.classList.remove('golden-filters__active');
+
+    } else {
+      filterBlock.classList.add('golden-filters__active');
+      button.classList.add('golden-filter-button__active');
+    }
+  });
+
+  mainBlock.insertBefore(button, header);
 }
--- wp-content/themes/optovicom/assets/scripts/modules/loadMore.js
+++ wp-content/themes/optovicom/assets/scripts/modules/loadMore.js
@@ -1,65 +1,64 @@
 function isAnyPartOfElementInViewport(el) {
 
-    const rect = el.getBoundingClientRect();
-    // DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
-    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-    const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
-    const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
-    const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+  const rect = el.getBoundingClientRect();
+  const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+  const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+  const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+  const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
 
-    return (vertInView && horInView);
+  return (vertInView && horInView);
 }
 
 let $flag = false;
 
 function loadMoreProducts(el, urls) {
-    let button = el,
-        data = {
-            'action': 'optovicom_load_more_ajax_callback',
-            'query': globalVars.posts,
-            'page': globalVars.current_page,
-        };
+  let button = el,
+    data = {
+      'action': 'optovicom_load_more_ajax_callback',
+      'query': globalVars.posts,
+      'page': globalVars.current_page,
+    };
 
-    $.ajax({
-        url: urls.ajaxUrl,
-        data: data,
-        type: 'POST',
-        beforeSend: function () {
-            button.text('Загрузка...');
-        },
-        success: function (data) {
-            if (data) {
-                button.prev().append(data);
-                button.text('Загрузить еще');
-                globalVars.current_page++;
-                $flag = false;
+  $.ajax({
+    url: urls.ajaxUrl,
+    data: data,
+    type: 'POST',
+    beforeSend: function () {
+      button.text('Загрузка...');
+    },
+    success: function (data) {
+      if (data) {
+        button.prev().append(data);
+        button.text('Загрузить еще');
+        globalVars.current_page++;
+        $flag = false;
 
-                if (parseInt(globalVars.current_page) === parseInt(globalVars.max_page))
-                    button.remove();
-            } else {
-                button.remove();
-            }
-        },
-    });
+        if (parseInt(globalVars.current_page) === parseInt(globalVars.max_page))
+          button.remove();
+      } else {
+        button.remove();
+      }
+    },
+  });
 }
 
 jQuery(document).ready(($) => {
-    const urls = {
-        homeUrl: $('.site-url').attr('data-src'),
-        rootDirUrl: $('.tpl-url').attr('data-src'),
-        ajaxUrl: $('.ajax-url').attr('data-src'),
-    };
+  const urls = {
+    homeUrl: $('.site-url').attr('data-src'),
+    rootDirUrl: $('.tpl-url').attr('data-src'),
+    ajaxUrl: $('.ajax-url').attr('data-src'),
+  };
 
-    const loadBtn = document.getElementById('load-more');
-    const $loadBtn = jQuery('#load-more');
+  const loadBtn = document.getElementById('load-more');
+  const $loadBtn = jQuery('#load-more');
 
-    $loadBtn.length > 0 && $loadBtn.click(function () {
-        loadMoreProducts($(this), urls);
-    });
+  $loadBtn.length > 0 && $loadBtn.click(function () {
+    loadMoreProducts($(this), urls);
+  });
 
-    loadBtn && jQuery(window).on('scroll', function () {
-        if (isAnyPartOfElementInViewport(loadBtn) && $.active < 1) {
-            loadMoreProducts($loadBtn, urls);
-        }
-    });
+  loadBtn && jQuery(window).on('scroll', function () {
+    if (isAnyPartOfElementInViewport(loadBtn) && $.active < 1) {
+      loadMoreProducts($loadBtn, urls);
+    }
+  });
 });
