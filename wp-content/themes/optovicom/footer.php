3b824e43ebf2f4bc32041b85b8d8fc8239c2c9d8
Nikita Lobach <nikita.lobach@beetroot.se>
2020-06-11 18:38:13 +0300

    Adjusted footer markup + added new fields to Theme Settings

--- wp-content/themes/optovicom/footer.php
+++ wp-content/themes/optovicom/footer.php
@@ -8,94 +8,99 @@
  *
  */
 
-$logo = get_header_image() ? '<img src="' . get_header_image() . '" class="footer__logo" alt="' . get_bloginfo('name') . '">' : get_bloginfo('name');
-$content = get_field('footer_content', 'option');
-$col1_title = get_field('footer_menu_column_title', 'option');
-$col2_title = get_field('footer_social_column_title', 'option'); ?>
+$footer = [
+  'logo'    => get_header_image() ? '<img src="' . get_header_image() . '" class="footer__logo" alt="' . get_bloginfo('name') . '">' : get_bloginfo('name'),
+  'content' => get_field('footer_content', 'option'),
+  'titles'  => [
+    'menu'   => get_field('footer_menu_column_title', 'option'),
+    'social' => get_field('footer_social_column_title', 'option'),
+  ],
+  'menu'    => get_field('footer_menu', 'option'),
+  'social'  => get_field('footer_social', 'option')
+]; ?>
 
 </div><!-- #content -->
 
 <footer id="footer-container" class="footer site-footer" role="contentinfo">
   <div class="footer-inner container">
     <div class="footer-top">
-        <?php if ($logo || $content) : ?>
-          <div class="column column-content">
-            <a href="<?= home_url('/'); ?>" class="footer__logo-wrapper"><?= $logo; ?></a>
-              <?php if ($content) : ?>
-                <div class="footer-content"><?= $content; ?></div>
-              <?php endif; ?>
-          </div>
-        <?php endif; ?>
+
+      <?php if ($footer['logo'] || $footer['content']) : ?>
+        <div class="column column-content">
+          <a href="<?= home_url('/'); ?>" class="footer__logo-wrapper"><?= $footer['logo']; ?></a>
+
+          <?php if ($footer['content']) : ?>
+            <div class="footer-content"><?= $footer['content']; ?></div>
+          <?php endif; ?>
+
+        </div>
+      <?php endif; ?>
+
       <div class="column column-menu">
-        <div class="golden-footer-title">
-          Телефон:
-        </div>
-        <div class="golden-footer-text">
-          +380731426969
-        </div>
-        <div class="golden-footer-title">
-          Почта:
-        </div>
-        <div class="golden-footer-text">
-          info@optovicom.com
-        </div>
-        <div class="golden-footer-title">
-          Адрес:
-        </div>
-        <div class="golden-footer-text">
-          Украина, Одесская область, Одесса, Промрынок 7
-        </div>
+
+        <?php foreach ($footer['menu'] as $menu_item) : ?>
+          <p class="title"><?= $menu_item['title']; ?></p>
+          <p class="text"><?= $menu_item['text']; ?></p>
+        <?php endforeach; ?>
+
       </div>
 
+      <?php if ($footer['titles']['social'] || !empty($footer['social'])) : ?>
+        <div class="column column-social">
 
-        <?php if ($col2_title || have_rows('footer_social', 'option')) : ?>
-          <div class="column column-social">
-              <?php if ($col2_title) : ?>
-                <p class="column__title"><?= $col2_title; ?></p>
-              <?php endif; ?>
+          <?php if ($footer['titles']['social']) : ?>
+            <p class="column__title"><?= $footer['titles']['social']; ?></p>
+          <?php endif; ?>
 
-              <?php if (have_rows('footer_social', 'option')) : ?>
-                <div class="footer-social-wrapper">
-                  <ul class="footer-social">
-                      <?php while (have_rows('footer_social', 'option')) : the_row();
-                          $icon = get_sub_field('fa_icon');
-                          $link = get_sub_field('link'); ?>
-                          <?php if ($link === 'tel:+380931426969') : ?>
-                              <?php continue; ?>
-                          <?php endif; ?>
-                        <li class="social__item">
-                          <a rel="nofollow" href="<?= $link; ?>" target="_blank"><?= $icon; ?></a>
-                        </li>
-                      <?php endwhile; ?>
-                  </ul>
-                </div>
-              <?php endif; ?>
-          </div>
-        <?php endif; ?>
+          <?php if (!empty($footer['social'])) : ?>
+            <div class="footer-social-wrapper">
+              <ul class="footer-social">
+
+                <?php foreach ($footer['social'] as $s_item) : ?>
+
+                  <?php if ($s_item['link'] === 'tel:+380931426969') {
+                    continue;
+                  } ?>
+
+                  <li class="social__item">
+                    <a rel="nofollow" href="<?= $s_item['link']; ?>" target="_blank"><?= $s_item['fa_icon']; ?></a>
+                  </li>
+
+                <?php endforeach; ?>
+
+              </ul>
+            </div>
+          <?php endif; ?>
+
+        </div>
+      <?php endif; ?>
+
     </div>
     <div class="footer-bottom">
       <p class="copyright">
-        &copy; <?= get_bloginfo('name') . ' ' . date('Y') . ' все права защищены.'; ?></p>
-        <?php if (has_nav_menu('footer_menu')) : ?>
-          <nav class="footer-nav footer-menu-wrapper">
-              <?php wp_nav_menu([
-                  'theme_location' => 'footer_menu',
-                  'menu_id'        => 'footer-sub-menu',
-                  'menu_class'     => 'footer-sub-menu',
-                  'container'      => false,
-                  'walker'         => new optovicom_navwalker()
-              ]); ?>
-          </nav>
-        <?php endif; ?>
+        &copy; <?= get_bloginfo('name') . ' ' . date('Y') . ' ' . __('все права защищены.', THEME_TD); ?></p>
+
+      <?php if (has_nav_menu('footer_menu')) : ?>
+        <nav class="footer-nav footer-menu-wrapper">
+          <?php wp_nav_menu([
+            'theme_location' => 'footer_menu',
+            'menu_id'        => 'footer-sub-menu',
+            'menu_class'     => 'footer-sub-menu',
+            'container'      => false,
+            'walker'         => new optovicom_navwalker()
+          ]); ?>
+        </nav>
+      <?php endif; ?>
+
     </div>
   </div>
 </footer>
 <?php wp_footer(); ?>
+
 <div class="viber-social">
   <a class="viber-pc" href="viber://chat?number=+380931426969"></a>
   <a class="viber-phone" href="viber://add?number=380931426969"></a>
 </div>
-<script type="text/javascript"
-        src="//optovicom.com/wp-content/themes/optovicom/assets/scripts/golden-footer.js"></script>
+<script src="<?= get_template_directory_uri() ?>/assets/scripts/golden-footer.js"></script>
 </body>
 </html>
